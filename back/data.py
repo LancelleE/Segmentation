35ded130b77c7451096f6e55249b7e6d101b95c9
etiennelancelle <elancelle.pro@gmail.com>
2024-02-04 23:05:45 +0100

    edits

--- back/data.py
+++ back/data.py
@@ -9,19 +9,37 @@ class RawData:
         self.data_hot_encoded = pd.DataFrame(dataset.select_dtypes(exclude=['int16', 'int32', 'int64', 'float16', 'float32', 'float64']))
         self.final_dataset = pd.DataFrame(dataset)
 
+    def filter_data(self, exclude_columns):
+        if exclude_columns:
+            keep_cols = [col for col in self.data if col not in exclude_columns]
+            self.data_filtered = self.data[keep_cols]
+        else:
+            self.data_filtered = self.data
+
 
     def standardize_numerical_values(self):
-        df_num = self.data.select_dtypes(include=['int16', 'int32', 'int64', 'float16', 'float32', 'float64'])
+        df_num = self.data_filtered.select_dtypes(include=['int16', 'int32', 'int64', 'float16', 'float32', 'float64'])
         if not df_num.empty:
             scaler = StandardScaler()
             self.data_standardized = pd.DataFrame(scaler.fit_transform(df_num), columns=df_num.columns)
 
-    def encode_qualitative(self):
-        df_char = self.data.select_dtypes(exclude=['int16', 'int32', 'int64', 'float16', 'float32', 'float64'])
-        if not df_char.empty:
+    def encode_qualitative(self, exclude_columns=None):
+        if exclude_columns is None:
+            exclude_columns = []
+        df_char = self.data_filtered.select_dtypes(exclude=['int16', 'int32', 'int64', 'float16', 'float32', 'float64'])
+        
+        # Exclude specified columns from one-hot encoding
+        columns_to_encode = [col for col in df_char.columns if col not in exclude_columns]
+        if len(columns_to_encode) > 0:
             encoder = OneHotEncoder(handle_unknown='ignore', sparse_output=False)
-            self.data_hot_encoded = pd.DataFrame(encoder.fit_transform(df_char),
-                                                 columns=encoder.get_feature_names_out(df_char.columns))
+            encoded_data = encoder.fit_transform(df_char[columns_to_encode])
+
+            # Create a DataFrame with the encoded features and concatenate it with the excluded columns
+            encoded_df = pd.DataFrame(encoded_data, columns=encoder.get_feature_names_out(columns_to_encode))
+            self.data_hot_encoded = pd.concat([encoded_df, df_char[exclude_columns]], axis=1, sort=False)
+
+            
+
 
 
     def merge_datasets(self):
--- main.py
+++ main.py
@@ -3,25 +3,35 @@ import pandas as pd
 from back.data import RawData
 
 # Function to handle computation
-def perform_computation(df, choix_colonnes, toggle_standardise, toggle_hot_encoding):
+def perform_computation(df, choix_colonnes, toggle_standardise, toggle_hot_encoding, id_column):
     if not choix_colonnes:
         # If no features selected, return the original dataframe
         return df
 
-    filtered_df = df.filter(choix_colonnes)
-    data_object = RawData(filtered_df)
+    
+    # filtered_df = df.filter(choix_colonnes)
+    data_object = RawData(df)
+    data_object.filter_data(choix_colonnes)
+
+    if id_column in data_object.data_filtered.columns:
+            data_object.data_filtered.set_index(id_column, inplace=True)
 
     if toggle_standardise:
         data_object.standardize_numerical_values()
 
     if toggle_hot_encoding:
-        data_object.encode_qualitative()
+        data_object.encode_qualitative(exclude_columns=[id_column])
 
     # Check if neither standardization nor hot encoding is selected
     if not toggle_standardise and not toggle_hot_encoding:
-        return filtered_df
+        return df
 
     data_object.merge_datasets()
+
+    # Preserve the ID column as an index
+    if id_column in data_object.final_dataset.columns:
+            data_object.final_dataset.set_index(id_column, inplace=True)
+
     return data_object.final_dataset
 
 
@@ -41,10 +51,13 @@ if uploaded_file:
     toggle_standardise = st.sidebar.checkbox('Standardise?', value=True)
     toggle_hot_encoding = st.sidebar.checkbox('Hot encoding?', value=True)
 
+    options.append(None)
+    id_column = st.sidebar.selectbox('Select the column containing the ID', options=options)
+
     compute_button = st.sidebar.button('Compute')
 
     if compute_button:
         with st.spinner("Computing..."):
-            final_dataset = perform_computation(df, choix_colonnes, toggle_standardise, toggle_hot_encoding)
+            final_dataset = perform_computation(df, choix_colonnes, toggle_standardise, toggle_hot_encoding, id_column)
             st.success("Computation complete!")
             st.write(final_dataset)
